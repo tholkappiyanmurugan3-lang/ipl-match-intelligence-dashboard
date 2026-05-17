ebd59643cd6a5822627feeefdfc1aa45412c73f7
Tholkappiyan <tholkappiyanmurugan3@gmail.com>
2026-05-17 13:26:54 +0530

    clean project structure for deployment

--- app.py
+++ app.py
@@ -122,5 +122,8 @@ def phase_analysis():
 # -----------------------------------
 # RUN SERVER
 # -----------------------------------
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
